820db08f16274e971cea539452e825132cb67ae0
VandycKing <mygitglobal@gmail.com>
2024-06-29 17:48:59 +0100

    Implements read_db_creds method

--- database_utils.py
+++ database_utils.py
@@ -3,11 +3,20 @@ import yaml
 
 class DatabaseConnector:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.db_config = None
 
     def init_db_engine(self):
         pass
 
     def read_db_creds(self):
-        pass
+        try:
+            with open('db_config.yaml', 'r') as db_config_file:
+                self.db_config = yaml.safe_load(db_config_file)
+                return self.db_config
+        except FileNotFoundError:
+            print("Error: The file 'db_creds.yaml' does not exist.")
+        except yaml.YAMLError as e:
+            print(f"Error reading the YAML file: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
