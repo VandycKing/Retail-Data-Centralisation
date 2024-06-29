88aa6891030693af023d5206747c053c4e719216
VandycKing <mygitglobal@gmail.com>
2024-06-29 15:05:48 +0100

    Creates script to connect and upload data to database

---
+++ database_utils.py
@@ -0,0 +1,13 @@
+import yaml
+
+
+class DatabaseConnector:
+
+    def __init__(self) -> None:
+        pass
+
+    def init_db_engine(self):
+        pass
+
+    def read_db_creds(self):
+        pass
