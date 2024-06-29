25b8909e403962a800e98ee14c21a37f70dd65e4
VandycKing <mygitglobal@gmail.com>
2024-06-29 18:17:47 +0100

    Implements init_db_engine  method

--- database_utils.py
+++ database_utils.py
@@ -1,13 +1,30 @@
 import yaml
+import sqlalchemy
 
 
 class DatabaseConnector:
 
     def __init__(self):
         self.db_config = None
+        self.engine = None
 
     def init_db_engine(self):
-        pass
+        if self.db_config:
+            try:
+                # Constructs database url
+                db_url = db_url = (
+                    f"postgresql://{self.db_config['RDS_USER']}:"
+                    f"{self.db_config['RDS_PASSWORD']}@"
+                    f"{self.db_config['RDS_HOST']}:"
+                    f"{self.db_config['RDS_PORT']}/"
+                    f"{self.db_config['RDS_DATABASE']}"
+                )
+
+                # Creates SQLAlchemy engine
+                self.engine = sqlalchemy.create_engine(db_url)
+                print("Database engine initialized successfully.")
+            except Exception as e:
+                print(f"Error initializing database engine: {e}")
 
     def read_db_creds(self):
         try:
